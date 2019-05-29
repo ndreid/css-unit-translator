19e1c35ed427cfc4773fc924c3fbcdf649e56cb6
ndreid <48798331+ndreid@users.noreply.github.com>
2019-05-29 10:22:40 -0500

    Add decimals parameter

--- index.js
+++ index.js
@@ -224,7 +224,7 @@ export default class CSSUnitTranslator {
     }
   }
 
-  convert(value, from, to) {
+  translate(value, from, to, decimals) {
     if (from === to)
       return value
 
@@ -232,133 +232,13 @@ export default class CSSUnitTranslator {
     if (!this.formulas[units])
       return undefined
 
-      console.log(this._c, this._c.fontSize, this._c.rootFontSize)
-
-    return this.formulas[units](value)
+    let result = this.formulas[units](value)
+    if (isNaN(result))
+      return result
+    
+    if (!isNaN(decimals))
+      result = Math.round(result * 10 ** decimals) / 10 ** decimals
+    
+    return result + to
   }
-
-  // convert (options) {
-  //     /* eslint-disable sort-keys */
-  //     const formulas = {
-  //         'ch-cm': options.value * 0.21087588,
-  //         'ch-em': options.value * 0.5,
-  //         'ch-ex': options.value / 0.9,
-  //         'ch-in': options.value * 0.083022,
-  //         'ch-mm': options.value * 2.1087588,
-  //         'ch-pc': options.value * 0.5,
-  //         'ch-pt': options.value * 5.977584,
-  //         'ch-px': options.value * options.base * 0.5,
-
-  //         'cm-ch': options.value / 0.21087588,
-  //         'cm-em': options.value / 0.42175176,
-  //         'cm-ex': options.value / 0.189788292,
-  //         'cm-in': options.value * 0.39,
-  //         'cm-mm': options.value * 10,
-  //         'cm-pc': options.value / 0.42175176,
-  //         'cm-pt': options.value * 28.3464566929,
-  //         'cm-%': options.value / options.base * 100 / 2.54 * options.dpi,
-  //         'cm-px': options.value / 2.54 * options.dpi,
-
-  //         'em-ch': options.value / 0.5,
-  //         'em-cm': options.value * 0.42175176,
-  //         'em-ex': options.value / 0.45,
-  //         'em-in': options.value * 0.166044,
-  //         'em-mm': options.value / 0.237106301584,
-  //         'em-pc': options.value,
-  //         'em-pt': options.value * 11.955168,
-  //         'em-%': options.value * 100,
-  //         'em-px': options.value * options.base,
-
-  //         'ex-ch': options.value * 0.9,
-  //         'ex-cm': options.value * 0.189788292,
-  //         'ex-em': options.value * 0.45,
-  //         'ex-in': options.value * 0.0747198,
-  //         'ex-mm': options.value * 1.89788292,
-  //         'ex-pc': options.value * 0.45,
-  //         'ex-pt': options.value * 5.3798256,
-  //         'ex-%': options.value * 45,
-  //         'ex-px': options.value * options.base * 0.45,
-
-  //         'in-ch': options.value / 0.083022,
-  //         'in-cm': options.value * 2.54,
-  //         'in-em': options.value / 0.166044,
-  //         'in-ex': options.value / 0.0747198,
-  //         'in-mm': options.value * 2.54 * 10,
-  //         'in-pc': options.value / 0.166044,
-  //         'in-pt': options.value / 0.014842519685,
-  //         'in-%': options.value / options.base * 100 * options.dpi,
-  //         'in-px': options.value * options.dpi,
-
-  //         'mm-ch': options.value / 2.1087588,
-  //         'mm-cm': options.value / 10,
-  //         'mm-em': options.value * 0.237106301584,
-  //         'mm-ex': options.value / 1.89788292,
-  //         'mm-in': options.value * 0.39 / 10,
-  //         'mm-pc': options.value / 4.42175176,
-  //         'mm-pt': options.value / 0.352777777778,
-  //         'mm-%': options.value / options.base * 100 / 2.54 * options.dpi / 10,
-  //         'mm-px': options.value / 2.54 * options.dpi / 10,
-
-  //         'pc-ch': options.value / 0.5,
-  //         'pc-cm': options.value * 0.42175176,
-  //         'pc-em': options.value,
-  //         'pc-ex': options.value / 0.45,
-  //         'pc-in': options.value * 0.166044,
-  //         'pc-mm': options.value * 4.42175176,
-  //         'pc-pt': options.value / 0.0836458341698,
-  //         'pc-%': options.value * 100,
-  //         'pc-px': options.value * options.base,
-
-  //         'pt-ch': options.value / 5.977584,
-  //         'pt-cm': options.value / 28.3464566929,
-  //         'pt-em': options.value / 11.955168,
-  //         'pt-ex': options.value / 5.3798256,
-  //         'pt-in': options.value * 0.014842519685,
-  //         'pt-mm': options.value * 0.352777777778,
-  //         'pt-pc': options.value * 0.0836458341698,
-  //         'pt-%': options.value / (options.base - 4) * 100,
-  //         'pt-px': options.value * 96 / 72,
-
-  //         '%-ch': options.value / 50,
-  //         '%-cm': options.value * options.base / 100 * 2.54 / options.dpi,
-  //         '%-em': options.value / 100,
-  //         '%-ex': options.value / 45,
-  //         '%-in': options.value * options.base / 100 / options.dpi,
-  //         '%-mm': options.value * options.base / 100 * 2.54 / options.dpi * 10,
-  //         '%-pc': options.value / 100,
-  //         '%-pt': options.value * (options.base - 4) / 100,
-  //         '%-px': options.value * options.base / 100,
-
-  //         'px-ch': options.value / options.base / 0.5,
-  //         'px-cm': options.value * 2.54 / options.dpi,
-  //         'px-em': options.value / options.base,
-  //         'px-ex': options.value / options.base / 0.45,
-  //         'px-in': options.value / options.dpi,
-  //         'px-mm': options.value * 2.54 / options.dpi * 10,
-  //         'px-pc': options.value / options.base,
-  //         'px-pt': options.value * 72 / 96,
-  //         'px-%': options.value / options.base * 100,
-  //     };
-  //     /* eslint-enable sort-keys */
-
-  //     const units = `${options.from}-${options.to}`;
-  //     console.log(units, options.value, options.value * 100)
-  //     const result = formulas[units];
-
-
-
-  //     if (isNaN(result)) {
-  //         return false;
-  //     }
-
-  //     return this.round(result, options.decimals) + options.to;
-  // }
-
-  // getUnits () {
-  //   return ['ch', 'cm', 'em', 'ex', 'in', 'mm', 'pc', 'pt', '%', 'px'];
-  // }
-
-  // round (number, decimals) {
-  //   return Math.round(number * 10 ** decimals) / 10 ** decimals;
-  // }
 }
